b2e0ff4a446530f5a17259788f83560a0cc4ec99
FUJIKAWA-IKU <fujikawaiku.potepan@gmail.com>
2024-01-01 13:31:31 +0900

    トップページの一覧コンポーネント作成

---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import NextLink from "next/link";
+import {Card, CardBody, CardHeader, Heading, Text, LinkBox, LinkOverlay} from "@chakra-ui/react";
+
+type Props = any;
+
+export const ArticleCard: React.FC<Props> = ({data}) => {
+    return (
+        <LinkBox>
+            <LinkOverlay as={NextLink} href='show'>
+                <Card
+                    key={data.title}
+                    overflow='hidden'
+                    variant='outline'
+                >
+                    <CardHeader>
+                        <Heading size='lg'>{data.title}</Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <Text>{data.createdAt}</Text>
+                    </CardBody>
+                </Card>
+            </LinkOverlay>
+        </LinkBox>
+    )
+}
+
+// export default ArticleCard;
